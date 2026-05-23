6387bd263bb05614c4153470273a9c79b157d682
defUserName-404 <moshiur.r.nafi@gmail.com>
2026-05-23 17:39:42 +0600

    chore: update dependencies to support newer android studio versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,45 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.22"
-    id("org.jetbrains.intellij") version "1.17.2"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.16.0"
 }
 
-// Must match <id> in plugin.xml: com.defusername.flutter_l10n
 group = "com.defusername"
 version = "1.0.0"
 
 repositories {
     mavenCentral()
-}
-
-intellij {
-    version.set("2024.1")   // minimum IDE version to build against
-    type.set("IC")           // IC = IntelliJ Community; use "AI" for Android Studio build
-
-    // "Dart" plugin provides all Dart PSI classes:
-    //   DartStringLiteralExpression, DartMethodDeclaration,
-    //   DartClassDefinition, DartFormalParameterList, etc.
-    // Pin an IntelliJ 2024.1-compatible Dart plugin build.
-    plugins.set(listOf("Dart:241.14494.17"))
+    intellijPlatform {
+        defaultRepositories()
+        marketplace()
+    }
 }
 
 dependencies {
-    // JSON parsing for ARB files (org.json is NOT bundled with IntelliJ)
+    intellijPlatform {
+        intellijIdea("2025.3")
+        localPlugin("${project.rootDir}/build/deps/DartPlugin/Dart")
+    }
     implementation("org.json:json:20240303")
 }
 
+intellijPlatform {
+    pluginConfiguration {
+        name = "Flutter L10n"
+        version = project.version.toString()
+        ideaVersion {
+            sinceBuild = "253"
+            untilBuild = "253.*"
+        }
+    }
+    instrumentCode = true
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
 tasks {
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        // Plugin works on IntelliJ 2024.1+ and Android Studio Koala+
-        sinceBuild.set("241")
-        untilBuild.set("243.*")
-    }
-
-    // Produce a zip ready to install via "Install Plugin from Disk"
     buildPlugin {
         archiveBaseName.set("flutter-l10n")
     }
